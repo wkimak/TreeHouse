757f070caf17bf2ccda40af97e450c2ac2a656e4
William Kimak <31807815+wkimak@users.noreply.github.com>
2017-10-27 13:04:21 -0400

    Create flickr.js

---
+++ Flickr-API/flickr.js
@@ -0,0 +1,35 @@
+$("document").ready(function(){
+
+$("#searchBtn").on("click", function(event){
+event.preventDefault();
+
+//searchterm var declaration
+var searchTerm = $("#search").val();
+
+//url requested
+var url="https://api.flickr.com/services/feeds/photos_public.gne?tags=" + searchTerm + "&format=json&jsoncallback=?";
+
+
+//callback function
+function showPhotos(data){
+	var showHTML = "<ul>";
+$.each(data.items, function(index,photo){
+showHTML += "<li> <a href='" + photo.link + "''>";
+showHTML+= "<img src='" + photo.media.m + "'> </a> </li>"; 
+}); //each loop
+showHTML +="</ul>";
+$("#photos").html(showHTML);
+
+//photo div animation
+$("#photos").hide();
+$("#photos").fadeIn(1200);
+
+};// showPhotos function
+
+
+//getJSON method
+$.getJSON(url, showPhotos);
+
+}); //click event
+
+}); //ready function
